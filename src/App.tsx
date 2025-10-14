2dc8a67a505da4082afcaba82eb25fe602dc421f
Mohamed Elsisi <mohammedelsisi21@gmail.com>
2025-10-14 17:18:11 +0300

    added Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux"
 import FileOpendBar from "./components/FileOpendBar"
 import RecursiveComponent from "./components/RecursiveComponent"
 import ResizeablePanel from "./components/ResizeablePanel"
 import { FileTree } from "./data/FileTree"
+import type { RootState } from "./redux/store"
+import HomeWelcome from "./components/HomeWelcome"
 
 const App = () => {
+  const {openFile} = useSelector((store : RootState) => store.fileTree)
   return (
     <>
     <div className="h-screen flex items-start">
@@ -14,7 +18,7 @@ const App = () => {
       leftPanel={<div className="py-2">
         <RecursiveComponent FileTree={FileTree}/>
       </div>}
-      rightPanel={<FileOpendBar />}
+      rightPanel={openFile.length ? <FileOpendBar /> : <HomeWelcome/>}
       />
     </div>
 </>)
---
+++ src/components/HomeWelcome.tsx
@@ -0,0 +1,10 @@
+
+const HomeWelcome = () => {
+  return (
+    <div className="h-screen flex items-center justify-center">
+        <img src="/icons/vscode.svg" alt="vscode" className="w-72 h-72" />
+    </div>
+  )
+}
+
+export default HomeWelcome
