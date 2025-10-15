6cbdf5dbce88ee9ea24aa3b7c935bf128b6f56b5
Mohamed Elsisi <mohammedelsisi21@gmail.com>
2025-10-15 10:38:10 +0300

    Demo

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
   plugins: [react(),
     tailwindcss(),
   ],
-    base: process.env.NODE_ENV === 'production' ? '/vs-code-project/' : '/',
+    base: '/vs-code-project/',
 
 })
